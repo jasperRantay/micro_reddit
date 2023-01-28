0e762c3df3cb1174874a4d81327dec042bf9bd94
jasper <jasper@bootyard.com>
2023-01-28 16:25:03 +0800

    setting up index page and showing posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,25 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+  def show
+    @article = Article.find(params[:id])
+  end
+  def new
+
+  end
+  def create
+
+  end
+  def edit
+
+  end
+  def update
+
+  end
+
+  def delete
+
+  end
+
+  private
 end
