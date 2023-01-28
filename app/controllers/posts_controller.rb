f699d6f9e767093d2f3cc2f05ce19c1b6fe662db
jasper <jasper@bootyard.com>
2023-01-28 16:53:55 +0800

    adding comment feature

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -32,9 +32,12 @@ class PostsController < ApplicationController
     end
   end
 
-  def delete
-
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to root_path, status: :see_other
   end
+
 
   private
     def post_params
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_28_081540) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_28_084846) do
+  create_table "comments", force: :cascade do |t|
+    t.string "user"
+    t.text "body"
+    t.integer "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -18,4 +27,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_28_081540) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "posts"
 end
