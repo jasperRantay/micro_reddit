a7b883f44d348ab25545431fd4ddcefe26ebd1a3
jasper <jasper@bootyard.com>
2023-01-28 17:20:42 +0800

    working on errors

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,11 +3,11 @@ class CommentsController < ApplicationController
     def create
         @post = Post.find(params[:post_id])
         @comment = @post.comments.create(comment_params)
-        redirect_to article_path
+        redirect_to post_path
     end
     
     private
         def comment_params
-          params.require(:comment).permit(:commenter, :body)
+          params.require(:comment).permit(:user, :body)
         end
 end
